10342a27821e20d5168a3526e61e29d1374b2937
sandrinemfuranzima <75666150+sandrinemfuranzima@users.noreply.github.com>
2021-02-09 16:09:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ function getCrustCost() {
   var selectedCrust = document.getElementById("crust").value;
   return parseInt(selectedCrust);
 }
-
 function getNumber() {
   var selectedNumber = document.getElementById("numberofpizza").value;
   return parseInt(selectedNumber);
